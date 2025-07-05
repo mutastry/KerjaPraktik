3f54103d790c0819dab9aa1fe71a89d4c78c526b
mutastry <mutiaraastri2707@gmail.com>
2025-07-05 08:16:25 -0800

    tambah order status

---
+++ kerjapraktik/app/Enums/OrderStatus.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enums;
+
+enum OrderStatus: string
+{
+    case PENDING = 'pending';
+    case PENDING_PAYMENT = 'pending_payment';
+    case READY_FOR_PICKUP = 'ready_for_pickup';
+    case COMPLETED = 'completed';
+    case CANCELLED = 'canceled';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::PENDING => 'Pending',
+            self::PENDING_PAYMENT => 'Pending Payment',
+            self::READY_FOR_PICKUP => 'Ready for Pickup',
+            self::COMPLETED => 'Completed',
+            self::CANCELLED => 'Cancelled',
+        };
+    }
+
